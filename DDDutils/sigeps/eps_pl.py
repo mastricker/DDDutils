ff03881871b6d7e137cbb53b1b066d2fbcda379a
Markus Stricker <mail@markusstricker.de>
2015-12-10 08:50:07 +0100

    library for dealing with plastic strain data (output of option 19 of disloc)

---
+++ DDDutils/sigeps/eps_pl.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+author: Markus Stricker
+
+Functions written for the analysis of <eps_ij_000XXXXX.csv> files, which
+come out of disloc when choosing option 19 and give the plastic strain
+in voxels according to their swept area.
+"""
